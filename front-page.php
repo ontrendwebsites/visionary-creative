dde8055b07cf3fd34239b9c8b2558f40716a14cb
Andrew McDougal <andrew@ontrendwebsites.com>
2019-01-30 22:30:47 +1100

    Homepage tweaks
    Add ACF profile info.
    Style Profile.
    Align header with content.
    Home page turn off/on

--- front-page.php
+++ front-page.php
@@ -21,78 +21,54 @@ get_header(); ?>
 		<div id="content" class="site-content">
 			<!-- add skeleton responsive grid -->
 			<div class="wrap container content-area">
-				<main id="main" class="site-main" role="main">
+				
+				<?php
+				// the query
+				$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
 
-					<?php
-					if ( have_posts() ) :
+				<?php 
+				$count =1;
+				// open div before
+				echo '<div class="row masonry image-holder">';
 
-						/* Start the Loop */
-						while ( have_posts() ) : the_post();
+				if ( $wpb_all_query->have_posts() ) : while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+					<div class="column-custom column one-half">
+						<!-- thumbnail -->
+						<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail(); ?>
+						<!-- background hover -->
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+							<div class="hover" style="background-color:<?php the_field( 'hover_color' ); ?>"></div>
+						</a>
+						<div class="info">
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<h3><?php the_category(' / '); ?></h3>
+						</div>
+					</div>
+					<?php endif; ?>
 
-							/*
-							 * Include the Post-Format-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							 */
-							//get_template_part( 'template-parts/post/content', get_post_format() );
+						<?php 
+							//if($count % 2 == 0) {echo '</div><div class="row">';}
 
-						endwhile;
+							$count++;
+						?>
 
-					else :
+					<?php endwhile; ?>
+					<!-- end of the loop -->
 
-						get_template_part( 'template-parts/post/content', 'none' );
-
-					endif;
-					?>
-
-
-<?php 
-
-// the query
-$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
-
-<?php 
-$count =1;
-// open div before
-echo '<div class="row masonry image-holder">';
-
-if ( $wpb_all_query->have_posts() ) : while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-	<div class="column-custom column one-half">
-		<!-- thumbnail -->
-		<?php if ( has_post_thumbnail() ) : ?>
-		<?php the_post_thumbnail(); ?>
-		<!-- background hover -->
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-			<div class="hover" style="background-color:<?php the_field( 'hover_color' ); ?>"></div>
-		</a>
-		<div class="info">
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<h3><?php the_category(' / '); ?></h3>
+				</div>
+				<?php wp_reset_postdata(); ?>
+				<div class="home-profile">
+					<?php the_post_thumbnail( 'full' ); ?>
+					<h5><?php the_field('name'); ?></h5>
+					<?php the_field('details'); ?>
+				</div>
+			</div>
+			<?php else : ?>
+			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
 		</div>
 	</div>
-	<?php endif; ?>
-
-		<?php 
-			//if($count % 2 == 0) {echo '</div><div class="row">';}
-
-			$count++;
-		?>
-
-	<?php endwhile; ?>
-	<!-- end of the loop -->
-
-</div>
-
-	<?php wp_reset_postdata(); ?>
-
-<?php else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-</div>
-
-
-					</main><!-- #main -->
-				</div>
-			</div><!-- .wrap -->
+</div><!-- .wrap -->
 
 <?php get_footer(); ?>
--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -9,12 +9,17 @@
  */
 
 ?>
+
+<!-- show full page image on home if selected as YES in options -->
+    
+<?php if( get_field('full_page_image', 'option') ): ?>    
 <div class="custom-header">
 
-	<div class="custom-header-media">
-		<?php the_custom_header_markup(); ?>
-	</div>
+    <div class="custom-header-media">
+        <?php the_custom_header_markup(); ?>
+    </div>
 
-	<?php get_template_part( 'template-parts/header/site', 'branding-2' ); ?>
+    <?php get_template_part( 'template-parts/header/site', 'branding-2' ); ?>
 
-</div><!-- .custom-header -->
+</div><!-- .custom-header -->  
+<?php endif; ?>
